787751020907d0c531c06ab78a2eb7deee3b7899
sauravsir <84040804+sauravsir@users.noreply.github.com>
2022-05-16 13:57:16 +0530

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,14 @@
+// var emp = ["saurav","Ravi","Ajay"];
+// for(i=0;i<emp.length;i++){
+//     console.log(emp[i]);
+// }
+
+var i;
+var emp = new Array();
+emp[0] = "Arun";
+emp[1] = "Saurav";
+emp[2] = "John";
+
+for(i=0;i<emp.length;i++){
+    console.log(emp[i]);
+}
